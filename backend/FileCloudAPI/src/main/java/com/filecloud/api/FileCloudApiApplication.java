35dd91975e64a3beec77f916c9fd633744f4c24d
林永紳 <84758386+mute-coding@users.noreply.github.com>
2024-06-10 22:31:15 +0800

    專案第一版

---
+++ backend/FileCloudAPI/src/main/java/com/filecloud/api/FileCloudApiApplication.java
@@ -0,0 +1,27 @@
+package com.filecloud.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@SpringBootApplication
+public class FileCloudApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileCloudApiApplication.class, args);
+	}
+    @Bean
+    WebMvcConfigurer configurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry reg) {
+				reg.addMapping("/**")
+				.allowedOrigins("*")
+				.allowedMethods("GET", "POST", "PUT", "DELETE");
+			}
+		};
+	}
+
+}
---
+++ backend/FileCloudAPI/src/main/java/com/filecloud/api/repository/FileItemRepository.java
@@ -0,0 +1,28 @@
+package com.filecloud.api.repository;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Options;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
+
+import com.filecloud.api.model.FileItem;
+
+@Mapper
+public interface FileItemRepository {
+	@Insert("INSERT INTO FileList (FileName,FileType,FileUrl) VALUES (#{FileName},#{FileType},#{FileUrl})")
+	@Options(useGeneratedKeys = true,keyProperty = "id")
+	FileItem addFileItem(FileItem fileItem);
+	@Select("SELECT * FROM FileList")
+	List<FileItem> getALL();
+	@Delete("DELETE FROM FileList WHERE id = #{id} ")
+	void deleteItem(Integer id);
+	@Update("UPDATE FileList SET FileName = #{FileName} WHERE id = #{id}")
+	void updateFileName(@Param("id") Integer id, @Param("FileName") String fileName);
+	@Select("SELECT * FROM FileList WHERE id = #{id}")
+	List<FileItem> getFileName(@Param("id") Integer id) ;
+}
